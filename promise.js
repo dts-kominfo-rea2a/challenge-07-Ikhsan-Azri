c9d1017379a706e9e532321f58e33acaa2ad48ee
Ikhsan Azri <ikhsanazri99@gmail.com>
2022-06-12 23:55:51 +0700

    Pening ___________ dan Selesai

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (hasil) => {
+  try {
+    const theaterIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+    const result = theaterIXX.concat(theaterVGC);
+    return result.filter((el) => el.hasil === hasil).length;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 module.exports = {
   promiseOutput,
